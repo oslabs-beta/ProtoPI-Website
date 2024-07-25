565dae9ae72736f5ad25810aa3f0e5b5a32704e7
WellsOfLivingWater <{107329536}+{WellsOfLivingWater}@users.noreply.github.com>
2024-07-24 20:25:19 -0400

    fix: remove edit-this-page link from blog

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -50,8 +50,8 @@ const config = {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          // editUrl:
+          //   'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         theme: {
           customCss: './src/css/custom.css',
@@ -64,7 +64,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: 'img/protopi-social-card.jpg',
       navbar: {
         title: 'ProtoPI',
         logo: {
@@ -129,13 +129,19 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} OSLabs Beta. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} ProtoPI. Built with Docusaurus.`,
       },
       prism: {
         theme: prismThemes.github,
         darkTheme: prismThemes.dracula,
       },
     }),
+  stylesheets: [
+    {
+      href: 'https://fonts.googleapis.com/css2?family=Gloria+Hallelujah&display=swap',
+      type: 'text/css',
+    },
+  ],
 };
 
 export default config;
