ce76d669d5852c8495b2ccac9152a4d5e4dc81f6
WellsOfLivingWater <{107329536}+{WellsOfLivingWater}@users.noreply.github.com>
2024-08-11 20:21:39 -0400

    feat: replace Docusaurus home page content  with tech logos and ProtoPI feature descriptions

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -4,32 +4,29 @@ import styles from './styles.module.css';
 
 const FeatureList = [
   {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    title: 'Prototype While You Code',
+    Svg: require('@site/static/img/VSCode.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Our VS Code extension conveniently places all the API prototyping tools you need right in your favorite IDE.
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    title: 'OpenAPI Specification',
+    Svg: require('@site/static/img/OpenAPI.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        Describe your API(s) with the popular OpenAPI specification using ProtoPI's handy tools.
       </>
     ),
   },
   {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    title: 'Mock with Prism',
+    Svg: require('@site/static/img/Stoplight.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        ProtoPI has Stoplight Prism built in, so you can easily mock your API right from within VS Code.
       </>
     ),
   },
